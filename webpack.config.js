adb1ace30f0d815780a19923bd5ad7634989e35b
syedtalhaaskari <syedtalhaaskari@gmail.com>
2023-02-19 15:33:18 +0500

    Dev server and css style loaders configured

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,24 @@ const path = require("path");
 
 module.exports = {
     entry: './src/index.js',
+    mode: 'production',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 }
