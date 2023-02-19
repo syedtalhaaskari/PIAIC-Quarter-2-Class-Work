bb49728371fa6ef84225fe41b7ac7b2fa6598d61
syedtalhaaskari <syedtalhaaskari@gmail.com>
2023-02-19 14:57:04 +0500

    webpack config file added and configured

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: './src/inde.js',
+    entry: './src/index.js',
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'dist'),
