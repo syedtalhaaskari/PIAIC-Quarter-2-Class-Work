5d83c94ac72415bb37ad0a70ce1cdd845225b264
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-03-05 15:27:06 +0500

    a button is added for our todo app

--- uit/src/components/header/index.js
+++ uit/src/components/header/index.js
@@ -3,10 +3,15 @@ import { useState } from "react";
 const Header = (props) => { // props are custom properties which are provided by the parent component
     // the process of passing data from parent component to child component is called data binding
     const [input, setInput] = useState("");
+    const [todos, setTodos] = useState([]);
 
     const inputChange = (e) => {
         // This function runs everytime when input box's value change
         setInput(e.target.value);
+    }
+
+    const addTodo = () => {
+        console.log("Add Todo");
     }
 
     return (
@@ -15,6 +20,9 @@ const Header = (props) => { // props are custom properties which are provided by
                 value={input}
                 onChange={inputChange}
             />
+            <button onClick={addTodo}>Add</button>
+
+            <h5>{input}</h5>
         </div>
     )
 }
