abbdc7de857e384479eb6f61967fdffed9e4a5db
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-03-05 15:23:23 +0500

    inputChange function is added to change the state

--- uit/src/components/header/index.js
+++ uit/src/components/header/index.js
@@ -2,11 +2,19 @@ import { useState } from "react";
 
 const Header = (props) => { // props are custom properties which are provided by the parent component
     // the process of passing data from parent component to child component is called data binding
-    const [input, setInput] = useState("Talha");
+    const [input, setInput] = useState("");
+
+    const inputChange = (e) => {
+        // This function runs everytime when input box's value change
+        setInput(e.target.value);
+    }
 
     return (
         <div>
-            <input value={input} />
+            <input
+                value={input}
+                onChange={inputChange}
+            />
         </div>
     )
 }
