11b3bef2d2d0373bb043583598fed8feedb4c430
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-03-05 14:22:15 +0500

    Header Component Added

--- uit/src/index.js
+++ uit/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './components/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    Test
-  </React.StrictMode>
+	<>
+		<h1>Hello</h1>
+		Test
+		<Header />
+	</>
 );
