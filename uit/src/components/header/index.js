21684dc3b01321572779283bacb9730c5919cf18
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-03-05 14:36:58 +0500

    state added in header component

--- uit/src/components/header/index.js
+++ uit/src/components/header/index.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 const Header = () => {
+    const [state] = useState(0);
+
     return (
         <div>
-            Header
+            {state}
         </div>
     )
 }
