b48b12db36fa8065e542fe8c2f9cfc2764dd08c0
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-03-19 14:36:05 +0500

    Promise

--- javascriptEs6/js/app.js
+++ javascriptEs6/js/app.js
@@ -12,3 +12,51 @@ finally { // This block will always be called. It will not depend on either try 
     console.log("Finally Block");
 }
 console.log("Talha");
+
+// Promise
+console.log("-----------------------------------------------------------");
+console.log("Promise")
+let prom = new Promise((res, rej) => {
+    res("Success");
+})
+
+prom // This promise will get resolved
+    .then(val => { // This then will be called
+        console.log("Prom 1", val);
+        return "My Call Back 2"
+    })
+    .then(val2 => { // This then will be called after above then block
+        console.log("Prom 1 then", val2)
+    })
+    .catch(err => {
+        console.log("Prom 1", err);
+    })
+
+let prom2 = new Promise((res, rej) => {
+    rej("Failed");
+})
+
+prom2 // This promise will get rejected 
+    .then(val => {
+        console.log("Prom 2", val);
+    })
+    .catch(err => { // This will be called
+        console.log("Prom 2 Catch", err);
+        return "My Catch 1"
+    })
+    .then(err1Then1 => { // This will be called after above catch
+        console.log("Prom 2 Catch Then", err1Then1);
+    })
+
+let prom3 = new Promise((res, rej) => {
+    setTimeout(() => {
+        res("Success");
+    }, 4000)
+}).then(val => { // This will be called after 4 seconds
+    console.log("Prom 3 then", val);
+})
+
+console.log({ prom3 });
+
+// visit jsv9000.app or latentflip.com to understand how js manage tasks
+// Use Promise.allSettled() to call multiple apis at the same time
