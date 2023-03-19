a32c05486133087a8308e263b251c3edf187fef5
syedtalhaaskari <58172778+syedtalhaaskari@users.noreply.github.com>
2023-03-19 14:05:00 +0500

    try catch

---
+++ javascriptEs6/js/app.js
@@ -0,0 +1,11 @@
+// try catch
+console.log("-----------------------------------------------------------");
+console.log("try catch")
+let a;
+try {
+    a = akljsojaoiosapofs;
+}
+catch(e) {
+    console.log(e);
+}
+console.log("Talha");
